f886f99a199cb0cc310268d7df1a9a1ee501655a
valdez <leonardovaldez.sn@gmail.com>
2026-03-01 21:04:00 -0300

    Movie list in json

---
+++ src/br/com/alura/screenmatch/YearConversionErrorException.java
@@ -0,0 +1,14 @@
+package br.com.alura.screenmatch;
+
+public class YearConversionErrorException extends RuntimeException {
+    private String message;
+
+    public YearConversionErrorException(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String getMessage() {
+        return this.message;
+    }
+}
--- src/br/com/alura/screenmatch/main/MainQueries.java
+++ src/br/com/alura/screenmatch/main/MainQueries.java
@@ -1,5 +1,6 @@
 package br.com.alura.screenmatch.main;
 
+import br.com.alura.screenmatch.YearConversionErrorException;
 import br.com.alura.screenmatch.models.Title;
 import br.com.alura.screenmatch.models.TitleOmdb;
 import com.google.gson.FieldNamingPolicy;
@@ -7,19 +8,35 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import java.io.FileInputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 import java.util.Scanner;
 
 public class MainQueries {
     public static void main(String []args) throws IOException, InterruptedException {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Search a movie: ");
-        var search = scanner.nextLine();
+        String search = "";
+        List<Title> titles = new ArrayList<>();
+        Gson gson = new GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                .setPrettyPrinting()
+                .create();
+
+        while (!search.equalsIgnoreCase("quit")) {
+            System.out.println("Search a movie: ");
+            search = scanner.nextLine();
+
+            if(search.equalsIgnoreCase("quit")) {
+                break;
+            }
+        }
 
         Properties properties = new Properties();
         try (FileInputStream fis = new FileInputStream("config.properties")) {
@@ -28,35 +45,41 @@ public class MainQueries {
             System.out.println("Error loading config.properties");
         }
 
-        try {
         String apiKey =  properties.getProperty("omdb.apikey");
         String address = "http://www.omdbapi.com/?t=" + search.replace(" ", "+") + "&apikey=" + apiKey;
 
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(address))
-                .build();
-        HttpResponse<String> response = client
-                .send(request, HttpResponse.BodyHandlers.ofString());
+        try {
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(address))
+                    .build();
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
 
-        String json = response.body();
-        System.out.println(json);
+            String json = response.body();
+            System.out.println(json);
 
-        Gson gson = new GsonBuilder()
-                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
-                .create();
-
-        TitleOmdb omdbTitle = gson.fromJson(json, TitleOmdb.class);
-        System.out.println(omdbTitle);
+            TitleOmdb omdbTitle = gson.fromJson(json, TitleOmdb.class);
+            System.out.println(omdbTitle);
 
             Title title = new Title (omdbTitle);
             System.out.println("Converted title");
             System.out.println(title);
+
+            titles.add(title);
         } catch (NumberFormatException e) {
             System.out.println("The following error has occurred: ");
             System.out.println(e.getMessage());
+        } catch (IllegalArgumentException e) {
+            System.out.println("Argument error has occurred.");
+        } catch (YearConversionErrorException e) {
+            System.out.println(e.getMessage());
         }
 
-    }
+        System.out.println(titles);
 
+        FileWriter writer = new FileWriter("movies.json");
+        writer.write(gson.toJson(titles));
+        writer.close();
+    }
 }
